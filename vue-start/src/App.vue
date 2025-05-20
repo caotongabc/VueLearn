3359b968baedc9d910f8465c8fc506f62ad9ed9e
tong cao <tongcao@tongdeMacBook-Air.local>
2025-05-20 22:28:16 +0800

    250520-vue基础练手

--- vue-start/src/App.vue
+++ vue-start/src/App.vue
@@ -1,6 +1,21 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import {onMounted, ref} from 'vue'
+const count = ref(0);
+const isClick = ref(false);
+const changeCount = ()=> {
+  count.value++;
+  isClick.value = true;
+  // 测试本地存储localStorage
+  localStorage.setItem('count',count.value);
+}
+const refreshButton = ()=> {
+  isClick.value = false;
+}
+onMounted(()=> {
+  console.log('localStorage:',localStorage.getItem('count'))
+})
 </script>
 
 <template>
@@ -12,9 +27,10 @@ import TheWelcome from './components/TheWelcome.vue'
     </div>
   </header>
 
-  <main>
-    <!-- <TheWelcome /> -->
+  <main class="mainContainer">
     <span class="testSpan">测试文案</span>
+    <button @click="changeCount" :class="{isClick: isClick}">{{ count }}</button>
+    <button @click="refreshButton">刷新</button>
   </main>
 </template>
 
@@ -27,6 +43,28 @@ header {
   display: block;
   margin: 0 auto 2rem;
 }
+.mainContainer {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 300px;
+  /* background-color: blue; */
+  button {
+    width: 50px;
+    height: 35px;
+    transition: all 0.3s ease;
+    &:hover {
+      background-color: rgb(79, 33, 178);
+      transform: scale(1.2);
+      cursor: pointer;
+    }
+  }
+  .isClick {
+     border: 2px solid red;
+  }
+
+}
+
 
 @media (min-width: 1024px) {
   header {
--- vue-start/src/main.js
+++ vue-start/src/main.js
@@ -3,6 +3,6 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import HelloWorld from './components/HelloWorld.vue'
-
+// 挂载多个应用
 createApp(App).mount('#app')
 createApp(HelloWorld).mount('#app2')
